6bc5a6f590886384782fa9e83d4c72104d20a152
MahtabShah <mahtab.shah916@gmail.com>
2025-05-31 14:07:34 +0530

    fix: add feature or bug fix

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
-const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+
+// ✅ Correct way for Vite
+const allowedOrigin = import.meta.env.FRONTEND_URL || "http://localhost:5173";
 
 const socket = io(allowedOrigin, {
-  transports: ["websocket"], // optional, but better than polling
+  transports: ["websocket"],
   withCredentials: true,
 });
 
