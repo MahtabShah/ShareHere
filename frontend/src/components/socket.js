cb2abb1a5365c35873164e4becadbd2e30dfd7b2
MahtabShah <mahtab.shah916@gmail.com>
2025-05-31 13:23:11 +0530

    fix: add feature or bug fix

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,5 +1,11 @@
 import { io } from 'socket.io-client';
 const API = import.meta.env.VITE_API_URL;
 
-const socket = io(API); // change to your server URL in production
+import { io } from "socket.io-client";
+
+const socket = io("https://sharequeot.onrender.com", {
+  withCredentials: true
+});
+// change to your server URL in production
+
 export default socket;
