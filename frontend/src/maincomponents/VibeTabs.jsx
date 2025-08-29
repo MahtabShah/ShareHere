897f5c6a93051d2d83294d7da69a19dd36a44b8b
MahtabShah <mahtab.shah916@gmail.com>
2025-08-29 19:24:08 +0530

    Added Vite for Vercel deployment

--- frontend/src/maincomponents/VibeTabs.jsx
+++ frontend/src/maincomponents/VibeTabs.jsx
@@ -1,18 +1,38 @@
 import { useEffect, useState } from "react";
-import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import { useTheme } from "../context/Theme";
 import All_Post_Section from "../All_Post_Section";
 import { usePost } from "../context/PostContext";
-import { categories } from "../StanderdThings/StanderdData";
+import { categories, fontSize } from "../StanderdThings/StanderdData";
 import { Rank_Calculation } from "../context/PostContext";
 import StatusPage from "./StatusPage";
+import { useQuote } from "../context/QueotrContext";
+
+const Tab = ({ category, Key }) => {
+  const { bg1, bg2, bg3, text_clrH, text_clrM, text_clrL } = useTheme();
+
+  const TabStyle = {
+    border: `1px solid ${bg1}`,
+    color: Key === category ? bg1 : text_clrH,
+    minWidth: "max-content",
+    background: Key === category ? text_clrH : bg1,
+  };
+
+  return (
+    <>
+      <div className="px-3 py-1 rounded w-100" style={{ ...TabStyle }}>
+        {category.charAt(0).toUpperCase() + category.slice(1)}
+      </div>
+    </>
+  );
+};
 
 export const VibeTabs = () => {
-  const [Key, setKey] = useState("all");
   const { limit, page, fetch_n_posts, setPosts, post_loading } = usePost();
   const { bg1, bg2, bg3, text_clrH, text_clrM, text_clrL } = useTheme();
   const [loading, setLoading] = useState(true);
+  const [Key, setKey] = useState("all");
+  const { mobile_break_point, sm_break_point } = useQuote();
 
   useEffect(() => {
     (async () => {
@@ -32,42 +52,41 @@ export const VibeTabs = () => {
     })();
   }, [Key]);
 
+  const TabStyle = {
+    fontSize: "16px",
+    zIndex: 100000,
+    left: `${mobile_break_point ? "0px" : sm_break_point ? "78px" : "248px"}`,
+    right: `4px`,
+    cursor: "pointer",
+    background: bg2,
+  };
+
   return (
     <>
-      <div className="vibeTabs mt-5">
+      <div className="vibeTabs  position-relative mt-5 h-100 overflow-auto">
+        <div
+          className="p-2 position-fixed d-flex gap-2 overflow-auto none-scroller"
+          style={{ ...TabStyle }}
+        >
+          {categories.map(({ key, title }) => (
+            <div key={title} onClick={() => setKey(key)}>
+              <Tab category={key} Key={Key} />
+            </div>
+          ))}
+        </div>
+
         <div className="w-100 d-flex">
           <StatusPage />
         </div>
 
-        <Tabs
-          id="controlled-tab-example"
-          activeKey={Key}
-          onSelect={(k) => setKey(k)}
-          className="border-0 d-flex position-fixed mt-5 top-0 gap-3 flex-nowrap none-scroller p-2 overflow-auto"
-          transition={false}
-          style={{
-            "--bg1": bg1,
-            "--bg2": bg2,
-            "--tc1": text_clrH,
-            "--tc2": text_clrM,
-            width: "100%",
-            zIndex: 1000,
-            background: bg2,
-          }}
-        >
-          {categories.map(({ key, title }) => (
-            <Tab
-              eventKey={key}
-              title={title}
-              key={key}
-              className="border-0 py-0"
-            >
-              {Key === key && (
+        {categories.map(
+          ({ key, title }) =>
+            Key === key && (
+              <div key={title}>
                 <All_Post_Section loading={loading} category={key} />
-              )}
-            </Tab>
-          ))}
-        </Tabs>
+              </div>
+            )
+        )}
       </div>
     </>
   );
